78e9a5c43f99f5d7079a0e20f3774fa1e8371691
Ariful Haque <arifulh@gmail.com>
2011-07-17 22:10:47 -0400

    SongView now adjusts its render when it determines its corresponding Song model is 'playing'

--- js/app/views/SongView.js
+++ js/app/views/SongView.js
@@ -1,12 +1,24 @@
 var SongView = Backbone.View.extend({
 	tagName : "li",
 	
+	// Each song model has a corresponding view. Song views
+	// are treated as subviews within PlaylistView.
 	initialize : function() {
+		_.bindAll(this, 'modify');
+		this.model.bind("change:playing", this.modify);
 		this.template = _.template($("#song-template").html());
 	},
-		
+
+	// Each song view listens to it's corresponding Song model
+	// to see if it is currently playing. If so, adjust the css.
+	modify : function(song) {
+		$(this.$("div.song")).addClass("playing");
+		return song;
+	},
+
 	render : function() {
 		$(this.el).html(this.template(this.model.toJSON()));	
 		return this;
 	}
+
 });
