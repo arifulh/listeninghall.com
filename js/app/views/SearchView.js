2f2a899c83859995746e0e3fc8f0f310f4a27361
Ariful Haque <arifulh@gmail.com>
2011-07-26 23:18:16 -0400

    Cleaned up SearchView.js

--- js/app/views/SearchView.js
+++ js/app/views/SearchView.js
@@ -1,45 +1,58 @@
 var SearchView = Backbone.View.extend({
-	el: $("#search"),
-	
-	events : {
-		"keypress #searchText" : "search",
-		"click li a"           : "add"
-	},
-	
-	initialize : function() {
-		_.bindAll(this, 'search');
-		this.display        = $("#searchResults");
-		this.input          = $("#searchText");
-		this.searchTemplate = _.template($("#search-template").html());
-		this.display.jScrollPane();
-	},
-	
-	search : function(e) {
-		if (e.which !== 13) return;
-		var query = this.input.val();
-		var url = "http://gdata.youtube.com/feeds/api/videos?v=2&format=5&q=" 
-				  + query + "&alt=json-in-script&callback=?";
-		
-		
-		var display = this.display;
-		var searchTemplate = this.searchTemplate;
-		
-		$("#searchResults ul").fadeOut("fast");
-		
-		
-		$.getJSON(url, function(data) {				
-			display.jScrollPane().data('jsp').getContentPane().html(
-				searchTemplate({ "results" : data.feed.entry })
-			);
-			display.data('jsp').reinitialise(function() {
-				$("#searchResults ul").fadeIn("fast");
-			});			
-		});		
-	},
-	
-	add : function(e) {
-		var sid = e.currentTarget.id;
-		$.publish("song/send", [ sid ]);
-	}
-	
+    el: $("#search"),
+
+    events: {
+        "keypress #searchText" : "search",
+        "click #searchButton"  : "search",
+        "click li a"           : "add"
+    },
+
+    // Gdata api request url will be built from these parameters. 
+    gdataParams: [
+        "http://gdata.youtube.com/feeds/api/videos?", 
+        "&v=2", 
+        "&format=5", 
+        "&alt=json-in-script", 
+        "&callback=?"
+    ],
+
+    // Store references to most used DOM elements.   
+    // Attach jScrollPane plugin to the search  
+    // display for custom scrollbars.
+    initialize: function () {
+        this.input    = $("#searchText");
+        this.display  = $("#searchResults").jScrollPane().data('jsp');
+        this.template = _.template($("#search-template").html());
+    },
+
+    // Bind the request callback to the SearchView object to simplify
+    // the code. Since we are using custom scrollbars, we will need
+    // to reinitialize the display it so it can adjust to the new content.
+    search: function (e) {
+        if (e.which !== 13 && e.which !== 1) return;
+        var query = this.buildQuery();
+        $.getJSON(query, _.bind(function (data) {
+            var results = this.template({"results": data.feed.entry});
+            this.display.getContentPane().html(results);
+            this.display.reinitialise();
+        }, this));
+    },
+
+    // When an item is selected from the search list, we want 
+    // to fade it out from the list, indicating it has been 
+    // added. Publish an event so that the Connection object 
+    // can send a song-queue request to the server.
+    add: function (e) {
+        var sid = e.currentTarget.id;
+        $("li#" + sid).fadeOut();
+        this.display.reinitialise();
+        $.publish("song/send", [sid]);
+    },
+
+    // Build/return a gdata api request string from the
+    // parameters object, with our query text appened.
+    buildQuery: function () {
+        var query = this.input.val();
+        return this.gdataParams.join("").concat("&q=", query);
+    },
 });
