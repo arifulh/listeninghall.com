f45f83423f5f4897810d026528db2b54f6c157b0
Ariful Haque <arifulh@gmail.com>
2011-08-14 22:02:17 -0400

    Overhauled LoginView. There is now a more robust errorDialog module that can be used to display error messages to the user based on context (i.e password conflict will be shown near password input box). Also, since there are are a lot of elements within this view that need to be cached, those elements will now be cached closer to where they will be used most often (instead of just caching everything in the intialize function). This was mostly done with closures.

--- js/app/views/LoginView.js
+++ js/app/views/LoginView.js
@@ -1,5 +1,4 @@
 var LoginView = Backbone.View.extend({
-
     el: $("#loading"),
 
     events: {
@@ -11,36 +10,66 @@ var LoginView = Backbone.View.extend({
         $.subscribe('room/joined',         this.success);
         $.subscribe("room/requirePass",    this.requirePass);
         $.subscribe("room/conflict",       this.handleConflict);
-        $.subscribe('connection/progress', this.handleProgress);
+        $.subscribe('connection/progress', this.updateProgress);
     },
 
     // Store references to the DOM elements that we will need access frequently
     // for user input and animation. Also call 'checkUrlHash" once this view
     // is initialized to handle room parameters right away.
     initialize: function () {
-        _.bindAll(this, 'login', 'subscribe', 'success', 'checkUrlHash', 'handleProgress');
-        this.$nickInput   = $("#loginNick");
-        this.$roomInput   = $("#loginRoom");
-        this.$passInput   = $("#loginPass");
-        this.$loginButton = $("#loginButton");
-        this.$progress    = $("#loginProgress");
-        this.create       = true; // Default, assume new room will be created.
+        _.bindAll(this, 'success', 'updateProgress', 'handleConflict');
+        this.progress  = this.progressBar();
+        this.showError = this.errorDialog();
+        this.create    = true; // Default, assume new room will be created.
+        this.cacheInputs();
         this.checkUrlHash();
         this.subscribe();
     },
 
-    // Grab login form information, and publish an event for the
-    // Connection object to attempt connect with the provided
-    // information. The "roomType" determines if a new room needs
-    // to be created or not.
+    // Cache input fields/buttons that will need to frequently 
+    // reference to perform error-checking/modification.
+    cacheInputs: function () {
+        this.$nickInput   = this.$("#loginNick");
+        this.$roomInput   = this.$("#loginRoom");
+        this.$passInput   = this.$("#loginPass");
+        this.$loginButton = this.$("#loginButton");
+    },
+
+    // Attempt to login to the server when the login button is clicked. 
+    // There are two functions defined within this function, 'isLoginEmpty'
+    // verifies the login fields, and 'connect' handles creating a signal
+    // for the Connection object to connect.
     login: function () {
-        var options = {
-            room : this.$roomInput.val(),
-            nick : this.$nickInput.val(),
-            pass : this.$passInput.val()
-        }
-        var roomType = this.create ? 'New' : '';
-        $.publish('connection/join' + roomType, [options]);
+
+        // Determine if login fields are empty. Alert the user if
+        // they are and return false. This function self-executes.
+        var isLoginEmpty = _.bind(function () {
+            var empty = false;
+            if (this.$roomInput.val() === "") {
+                this.showError("room", "Please enter a room name");
+                empty = true;
+            }
+            if (this.$nickInput.val() === "") {
+                this.showError("nick", "Please enter a nick name");
+                empty = true;
+            }
+            return empty;
+        }, this)();
+
+        // Grab login form information, and publish an event for the
+        // Connection object to signal it to connect. The "roomType" 
+        // determines if a new room needs to be created or not.
+        var connect = _.bind(function () {
+            var roomType = this.create ? 'New' : '';
+            $.publish('connection/join' + roomType, [{
+                room: this.$roomInput.val(),
+                nick: this.$nickInput.val(),
+                pass: this.$passInput.val()
+            }]);
+        }, this);
+
+        // If login is not empty, connect.
+        if (!isLoginEmpty) { connect(); }
     },
 
     // Upon succesful entrance, set the room as a url
@@ -48,7 +77,8 @@ var LoginView = Backbone.View.extend({
     success: function (room, nick) {
         window.location.hash = "/" + room;
         $(this.el).fadeOut("slow", function () {
-            $("#top").fadeIn("slow");
+            var $header = $("#top");
+            $header.fadeIn("slow");
         });
     },
 
@@ -61,47 +91,81 @@ var LoginView = Backbone.View.extend({
             this.create = false;
             this.$roomInput.val(roomParameter)
                            .addClass("set1")
-                           .keypress(function (){ return false });
+                           .keypress(function () { return false; });
         }
         this.$loginButton.text(this.create ? "Create" : "Join");
     },
 
-    // Update connection progress bar based on status 
-    // updates. Once connected (status === 5), hide progress 
-    // bar and display login dialog.
-    handleProgress: function (status) {
-        var percentage = {
-            1: 30, // status === 1 : Connecting
-            3: 75, // status === 3 : Authorizing
-            5: 100 // status === 5 : Successfully connected
+    // Create progress bar that updates based on Connection
+    // status. This function returns a closure that will use the
+    // the DOM references in the outer function. 
+    progressBar: function () {
+        var $progressBar = this.$("#loginProgress"),
+            $loginDialog = this.$("#loginDialog"),
+            $login       = this.$("#login");
+
+        // Update progress bar slider. If the slider reaches 100,
+        // fade the slider out and display the login dialog. Switch 
+        // classes to manually enforce vertical alignment
+        return function (amount) {
+            $progressBar.slider("value", amount);
+            if (amount === 100) {
+                $progressBar.fadeOut(function () {
+                    $loginDialog.fadeIn();
+                    $login.switchClass("valign1", "valign2", "fast");
+                });
+            }
         };
-        this.$progress.slider("value", percentage[status], true);
-        if (status === 5) {
-            this.$progress.fadeOut(function () {
-                // Switch classes to manually enforce vertical alignment
-                $("#login").switchClass("valign1", "valign2", "fast");
-                $("#loginDialog").fadeIn();
-            });
+    },
+
+    // Intercept connection status events, and update the progress bar.
+    updateProgress: function (status) {
+        switch (status) {
+            case Strophe.Status.CONNECTING     : this.progress(30);  break;
+            case Strophe.Status.AUTHENTICATING : this.progress(75);  break;
+            case Strophe.Status.CONNECTED      : this.progress(100); break;
         }
     },
 
-    // Alert user if they try to create a room that already exists,
-    // or use a nickname already in use, or if the password provided
-    // is incorrect.
-    handleConflict: function (conflict) {
-        var $display = {
-            "exists" : $("#room_conflict"),
-            "nick"   : $("#nick_conflict"),
-            "pass"   : $("#pass_conflict")
+    // Create a error dialog that will display error alerts to the 
+    // user based on the type of error. This function references the 
+    // specific DOM elements where each type of error will be shown.  
+    errorDialog: function () {
+        var error = {
+            "room": $("#room_conflict"),
+            "nick": $("#nick_conflict"),
+            "pass": $("#pass_conflict")
         };
-        $display[conflict].effect("pulsate", {times: 3});
+
+        // Closure that will use the appropriate DOM references in
+        // the outer function to display the error.
+        return function (type, errorText) {
+            var $element = error[type];
+            $element.text(errorText).effect("pulsate", {times: 3});
+        }
+    },
+
+    // Handle conflict messages from the Connection object. Determine
+    // the type of conflict, and pass it on to the showError object.
+    handleConflict: function (conflict) {
+        switch (conflict) {
+            case "roomExists":
+                this.showError("room", "Room already exists"); 
+                break;
+            case "nickExists":
+                this.showError("nick", "Nick already taken");  
+                break;
+            case "passWrong":
+                this.showError("pass", "Required/Incorrect");  
+                break;
+        };
     },
 
     // Display the password field if the room is protected. Also
     // adjust the vertical alignment of the login box since we just
     // added a new field.
     requirePass: function () {
-        $("#login").switchClass("valign2", "valign3", "fast");
-        $("#requirePass").fadeIn();
+        this.$("#login").switchClass("valign2", "valign3", "fast");
+        this.$("#requirePass").fadeIn();
     }
 });
