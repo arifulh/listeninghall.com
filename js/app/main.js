28e37e28d7d44ab586c4e5b038196b42cfe9808d
Ariful Haque <arifulh@gmail.com>
2011-08-26 14:39:51 -0300

    Minor formatting update.

--- js/app/main.js
+++ js/app/main.js
@@ -1,32 +1,32 @@
-$(function() {
-	var mainapp = {
-		init : function() {
-			// Collections
-			this.members 	  = new MemberList;
-			this.playlist 	  = new Playlist;	
-			
-			// Views
-			this.playlistView = new PlaylistView ({ collection: this.playlist });
-			this.playerView	  = new YTPlayerView ({ collection: this.playlist });
-			this.toolbarView  = new ToolbarView  ({ collection: this.members  });
-			this.searchView   = new SearchView;
-			this.chatView 	  = new ChatView;	
-			this.loginView 	  = new LoginView;
-			
-			// Connecton
-			Connection.initialize();
-		}
-	};
-	mainapp.init();    
+$(function () {
+    var mainapp = {
+        init: function () {
+        
+            // Collections
+            this.members      = new MemberList;
+            this.playlist     = new Playlist;
+
+            // Views
+            this.playlistView = new PlaylistView({collection: this.playlist});
+            this.playerView   = new YTPlayerView({collection: this.playlist});
+            this.toolbarView  = new ToolbarView({collection: this.members});
+            this.searchView   = new SearchView;
+            this.chatView     = new ChatView;
+            this.loginView    = new LoginView;
+
+            // Connecton
+            Connection.initialize();
+        }
+    };
+    mainapp.init();
 });
 
 // Global youtube-api callback.
 function onYouTubePlayerReady(playerid) {
-	$.publish('youtube/ready', [ playerid ]);	
+    $.publish('youtube/ready', [playerid]);
 }
 
 // Global youtube-api callback.
 function onYoutubeStateChange(state) {
-	$.publish('youtube/state', [ state ]);
+    $.publish('youtube/state', [state]);
 }
-
