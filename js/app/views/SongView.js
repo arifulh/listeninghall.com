99afd9c28344f1616de80f0c324d28678da5a8cf
Ariful Haque <arifulh@gmail.com>
2011-08-30 17:39:14 -0300

    Edited js/app/views/SongView.js via GitHub

--- js/app/views/SongView.js
+++ js/app/views/SongView.js
@@ -1,27 +1,27 @@
 var SongView = Backbone.View.extend({
-	tagName : "li",
-	
-    events : {
+    tagName: "li",
+
+    events: {
         "click .plskip a" : "skip"
     },
 
-	// Each song model has a corresponding view. Song views
-	// are treated as subviews within PlaylistView.
-	initialize : function() {
-		_.bindAll(this, 'modify');
-		this.model.bind("change:playing", this.modify);
-		this.template = _.template($("#song-template").html());
-	},
+    // Each song model has a corresponding view. Song views
+    // are treated as subviews within PlaylistView.
+    initialize: function () {
+        _.bindAll(this, 'modify');
+        this.model.bind("change:playing", this.modify);
+        this.template = _.template($("#song-template").html());
+    },
 
-	// Each song view listens to it's corresponding Song model
-	// to see if it is currently playing. If so, adjust the css.
-	modify : function(song) {
-		$(this.$("div.song")).addClass("playing");
-		return song;
-	},
-    
+    // Each song view listens to it's corresponding Song model
+    // to see if it is currently playing. If so, adjust the css.
+    modify: function (song) {
+        $(this.$("div.song")).addClass("playing");
+        return song;
+    },
+
     // Send out a skip-vote request, and fade out the vote link clicked.
-    skip : function(event) {
+    skip: function (event) {
         var $target = $(event.currentTarget)
         var $element = $target.parent();
         $.publish('song/voteSkip');
@@ -29,8 +29,8 @@ var SongView = Backbone.View.extend({
     },
 
     // Render template, return view object
-	render : function() {
-		$(this.el).html(this.template(this.model.toJSON()));
-		return this;
-	}
+    render: function () {
+        $(this.el).html(this.template(this.model.toJSON()));
+        return this;
+    }
 });
